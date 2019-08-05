5a4406943903c9e275189f6a2ab31fdf233aa83e
ÍõÈü <mr_ws_vip@163.com>
2019-08-05 17:41:39 +0800

    springboot完善页面

--- src/main/java/com/example/springbootstudy/controller/StudentInfoController.java
+++ src/main/java/com/example/springbootstudy/controller/StudentInfoController.java
@@ -4,10 +4,7 @@ package com.example.springbootstudy.controller;
 import com.example.springbootstudy.beans.Student;
 import com.example.springbootstudy.iservice.IStudentInfo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @Title:
@@ -22,8 +19,19 @@ public class StudentInfoController {
     @Autowired
     IStudentInfo studentInfoSVimpl;
 
-    @GetMapping(value = "/getStudentInfo/{stuId}")
-    public Student getStudentInfo(@PathVariable(value = "stuId") int stuId){
-        return studentInfoSVimpl.getStudentInfo(stuId);
+//    @GetMapping(value = "/getStudentInfo/{stuId}")
+//    public Student getStudentInfo(@PathVariable(value = "stuId") int stuId){
+//        return studentInfoSVimpl.getStudentInfo(stuId);
+//    }
+@PostMapping(value = "/getStudentInfo")
+public String getStudentInfo(@RequestParam(value = "studentId") int stuId,@RequestParam(value = "passWord")String passWord){
+    Student student = new Student();
+    student = studentInfoSVimpl.getStudentInfo(stuId);
+    String ps = student.getPassWord();
+    if(ps.equals(passWord)){
+        return "登陆成功";
+    }else{
+        return "登陆失败";
     }
+}
 }
