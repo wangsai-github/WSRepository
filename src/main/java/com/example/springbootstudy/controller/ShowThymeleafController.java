973d24468bf4b4b2e31f3841e045ccf72ffbab49
ÍõÈü <mr_ws_vip@163.com>
2019-08-05 17:48:38 +0800

    springboot提交完善

---
+++ src/main/java/com/example/springbootstudy/controller/ShowThymeleafController.java
@@ -0,0 +1,24 @@
+package com.example.springbootstudy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @Title:
+ * @Description:
+ * @author: wangsai
+ * @date 2019/8/5 16:27
+ */
+
+@Controller
+@RequestMapping(value = "/StudentInfoCon")
+public class ShowThymeleafController {
+    @GetMapping(value = "/login")
+    public ModelAndView login(){
+        ModelAndView login = new ModelAndView();
+        login.setViewName("Login");
+        return login;
+    }
+}
